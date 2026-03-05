316f4fffd06590042c93d5ecdf7cca0d59bb68be
Jansen,Chad C.M <chad.jansen@student.fontys.nl>
2026-03-05 12:18:22 +0100

    1

--- moviesapi/src/pages/Favorites.jsx
+++ moviesapi/src/pages/Favorites.jsx
@@ -3,7 +3,7 @@ import "../css/Favorites.css";
 function Favorites(){
     return <div className="favorites-empty">
         <h2>No favorites movies yet</h2>
-        <p>Start adding movies to your favorites </p>
+        <p>Start adding movies to your favorites!! </p>
     </div>
 
 }
